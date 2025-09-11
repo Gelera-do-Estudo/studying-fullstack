27b8c837eafc281a1b4651a8679fba6c57d37aae
Renan <renan_cesar2000@hotmail.com>
2025-09-10 21:20:17 -0300

    Start Project Login

---
+++ js/clock.js
@@ -0,0 +1,28 @@
+const time = document.querySelector('#clockTime');
+const todaysDate = document.querySelector('#todaysDate');
+
+function changeTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    const formatTime = `${hours}:${minutes}`;
+    time.textContent = formatTime;
+} 
+
+function showDate() {
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+    };
+    const theDate = new Date();
+    const formatDate = theDate.toLocaleDateString('pt-BR', options);
+
+    todaysDate.textContent = formatDate;
+}
+
+showDate();
+setInterval(changeTime, 1000);
