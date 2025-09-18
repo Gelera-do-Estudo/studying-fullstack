0ffc78a6379d4fed877f84d6fec25a2370a2fe27
Renan <renan_cesar2000@hotmail.com>
2025-09-17 22:33:49 -0300

    clock.js modified

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,6 @@ function changeTime() {
     const now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     const formatTime = `${hours}:${minutes}`;
